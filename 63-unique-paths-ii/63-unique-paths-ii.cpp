37cc206a8a8f32f7148ed0fa6ce244437c0c33fa
RAJAT <65409221+rajat2411@users.noreply.github.com>
2022-06-02 20:47:09 +0530

    Time: 4 ms (62.84%), Space: 7.7 MB (63.04%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    int solve(vector<vector<int>>& obstacleGrid,int m ,int n,vector<vector<int>>& dp){
+   /* int solve(vector<vector<int>>& obstacleGrid,int m ,int n,vector<vector<int>>& dp){
         
         if(m>=0 && n>=0 && obstacleGrid[m][n]==1){
             return 0;
@@ -56,4 +56,44 @@ public:
         return dp[m-1][n-1];
         
     }
+    */
+     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+        // vector<vector<int>>dp(m,vector<int>(n,-1));
+        // return solve(obstacleGrid,m-1,n-1,dp);
+        
+        // tabulation 
+        // int dp[m][n];
+         vector<int>prev(n,0);
+        for(int i=0;i<m;i++){
+            vector<int>curr(n,0);
+            for(int j=0;j<n;j++){
+                //base case
+                if(obstacleGrid[i][j]==1){
+                    curr[j]=0;
+                }
+               else if(i==0 && j==0){
+                    curr[j]=1;
+                }
+                
+                else{
+                    int up=0;
+                    int left=0;
+                    if(i>0){
+                         up=prev[j];
+                    }
+                    if(j>0){
+                         left=curr[j-1];
+                    }
+                    curr[j]=(left+up);
+                }
+            }
+            prev=curr;
+        }
+        return prev[n-1];
+        
+    }
+    
+    
 };
