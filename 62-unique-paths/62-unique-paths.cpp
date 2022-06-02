ec17c91da5d3b0cf8024622f703ab926e7f4ee51
RAJAT <65409221+rajat2411@users.noreply.github.com>
2022-06-02 18:58:13 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (98.59%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,26 +1,64 @@
 class Solution {
 public:
-    int uni(int m,int n,vector<vector<int>>&dp){
-        if(m==0 && n==0){
-            return 1;
-        }
+    
+    // memoization
+//     int uni(int m,int n,vector<vector<int>>&dp){
+//         if(m==0 && n==0){
+//             return 1;
+//         }
         
-        if(m<0 || n<0){
-            return 0;
-        }
-        if(dp[m][n]!=-1){
-            return dp[m][n];
-        }
+//         if(m<0 || n<0){
+//             return 0;
+//         }
+//         if(dp[m][n]!=-1){
+//             return dp[m][n];
+//         }
         
-        // accumulate
-        int left=uni(m,n-1,dp);
-        int up=uni(m-1,n,dp);
-        return dp[m][n]=left+up;
+//         // accumulate
+//         int left=uni(m,n-1,dp);
+//         int up=uni(m-1,n,dp);
+//         return dp[m][n]=left+up;
+//     }
+    
+    // int uniquePaths(int m, int n) {
+    //     // declareing dp of m*n 
+    //     vector<vector<int>>dp(m,vector<int>(n,-1));
+    //     return uni(m-1,n-1,dp);
+    // }
+    
+    
+    
+//     =======>>>>>> tabulation<<<<<<<<============
+     int uniquePaths(int m, int n) {
+        // declareing dp of m*n 
+        // vector<vector<int>>dp(m,vector<int>(n,-1));
+         int dp[m][n];
+        // return uni(m-1,n-1,dp);
+         
+         for(int i=0;i<m;i++){
+             for(int j=0;j<n;j++){
+                 if(i==0 && j==0){
+                     dp[i][j]=1;
+                 }
+                 else{
+                     int up=0;
+                     int left=0;
+                     if(j>0){
+                         left=dp[i][j-1];
+                     }
+                     if(i>0){
+                         up=dp[i-1][j];
+                     }
+                     dp[i][j]=left+up;
+                 }
+             }
+         }
+         return dp[m-1][n-1];
+         
     }
     
-    int uniquePaths(int m, int n) {
-        // declareing dp of m*n 
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return uni(m-1,n-1,dp);
-    }
+    
+    
+    
+    
 };
