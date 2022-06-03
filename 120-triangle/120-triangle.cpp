7515a344902cbf307a4ed56728e317f9a556690a
RAJAT <65409221+rajat2411@users.noreply.github.com>
2022-06-03 18:54:03 +0530

    Time: 7 ms (74.62%), Space: 8.9 MB (40.74%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+    
+    /* ======>>>>tabulation<<<<<<<========
+    class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        int dp[n][n];
+        for(int i=0;i<n;i++){
+            dp[n-1][i]=triangle[n-1][i];
+        }
+        
+        for(int j=n-2;j>=0;j--){
+            for(int k=j;k>=0;k--){
+                int d=triangle[j][k]+dp[j+1][k];
+                int dg=triangle[j][k]+dp[j+1][k+1];
+                dp[j][k]=min(d,dg);
+            }
+        }
+        return dp[0][0];
+    }
+};*/
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
         vector<int>prev(n);
