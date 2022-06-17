d117e1be3341d14664e37401572bfa549f99c6c9
RAJAT <65409221+rajat2411@users.noreply.github.com>
2022-06-17 16:56:13 +0530

    Time: 183 ms (35.20%), Space: 13.2 MB (86.79%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,42 +1,48 @@
 class Solution {
 public:
-    bool check(string &s1, string &s2)
-{
-    if(s1.length()-s2.length()!=1) return false;
-    int i=0,j=0;
-    while(i<s1.length())
-    {
-        if(s1[i]==s2[j])
-        {
-            i++;j++;
-        }
-        else
-            i++;
-    }
-    if(i==s1.length() && j== s2.length()) return true;
-    else return false;
-}
-static bool comp(string &s1,string &s2)
-{
-    if(s1.length()<s2.length()) return true;
-    else return false;
-}
-    int longestStrChain(vector<string>& words) {
-    int n = words.size();
-    sort(words.begin(),words.end(),comp);
-    vector<int>dp(n,1);
-    int maxi = 0;
-    for(int i=0;i<n;i++)
-    {
-        for(int prev=0;prev<i;prev++)
-        {
-            if(check(words[i],words[prev]) && dp[i]<dp[prev]+1)
-            {
-                dp[i] =1+dp[prev];
+   
+    
+    bool check(string &s1,string &s2){
+        if(s1.size()!=s2.size()+1) return false;
+        
+        int first=0;
+        int second=0;
+        while(first<s1.size()){
+            if(s1[first]==s2[second]){
+                first++;
+                second++;
+                
+            }
+            
+            else{
+                first++;
             }
         }
-       maxi = max(maxi,dp[i]); 
+        if(first==s1.size() && second==s2.size()) return true;
+        
+         return false;
+        
     }
-    return maxi;
+    
+     bool static comp(string &s1,string &s2){
+       return s1.size()<s2.size() ;
+    }
+    
+    int longestStrChain(vector<string>& words) {
+        int n=words.size();
+        sort(words.begin(),words.end(),comp);
+        int maxi=1;
+        vector<int>dp(n,1);
+        for(int i=0;i<n;i++){
+            for(int prev=0;prev<i;prev++){
+                if(check(words[i],words[prev])  && 1+dp[prev]>dp[i]){
+                    dp[i]=1+dp[prev];
+                }
+            }
+            if(dp[i]>maxi){
+                maxi=dp[i];
+            }
+        }
+        return maxi;
     }
 };
