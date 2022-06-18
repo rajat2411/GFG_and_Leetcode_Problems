ff7599441a3904346c501c3c83066892ac652f22
RAJAT <65409221+rajat2411@users.noreply.github.com>
2022-06-18 22:12:56 +0530

    Time: 21 ms (61.14%), Space: 8.2 MB (74.20%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -4,22 +4,38 @@ public:
         if(s.length()==0){
             return 0;
         }
-        unordered_set<int>sett;
-        int l=0;
-        int max_ans=INT_MIN;
-        for(int r=0;r<s.length();r++){
-            // check in set
-            if(sett.find(s[r])!=sett.end()){
-                while(l<r && sett.find(s[r])!=sett.end()){
-                    sett.erase(s[l]);
-                    l++;
-                }
-            }
-            sett.insert(s[r]);
-            max_ans=max(max_ans,r-l+1);
+        unordered_map<char,int>mpp;
+        int l=0,r=0;
+        int ans=-1e8;
+//         int max_ans=INT_MIN;
+//         for(int r=0;r<s.length();r++){
+//             // check in set
+//             if(sett.find(s[r])!=sett.end()){
+//                 while(l<r && sett.find(s[r])!=sett.end()){
+//                     sett.erase(s[l]);
+//                     l++;
+//                 }
+//             }
+//             sett.insert(s[r]);
+//             max_ans=max(max_ans,r-l+1);
             
+            
+//         }
+//         return max_ans;
+        
+        
+        // more optimised approach 
+        
+        while(r<s.length()){
+            if(mpp.find(s[r])!=mpp.end()){
+                l=max(l,mpp[s[r]]+1);
+            }
+            mpp[s[r]]=r;
+            ans=max(ans,r-l+1);
+            r++;
             
         }
-        return max_ans;
+        return ans;
+        
     }
 };
