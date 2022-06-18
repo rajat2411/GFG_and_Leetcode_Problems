b54a00230208b5246d79e3bb27fc7eebb855313e
RAJAT <65409221+rajat2411@users.noreply.github.com>
2022-06-18 14:40:32 +0530

    Time: 15 ms (75.88%), Space: 10.8 MB (52.87%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -4,15 +4,28 @@ public:
         int n=nums.size();
         vector<int > ans;
         // brute force approach 
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(nums[i]==target-nums[j]){
+        //             ans.push_back(i);
+        //             ans.push_back(j);
+        //             break;
+        //         }
+        //     }
+        // }
+        // return ans;
+        
+        // use of hashind 
+        unordered_map<int,int>mpp;
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]==target-nums[j]){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    break;
-                }
+            if(mpp.find(target-nums[i])!=mpp.end()){
+                ans.push_back(i);
+                ans.push_back(mpp[target-nums[i]]);
+                break;
             }
+            mpp[nums[i]]=i;
         }
         return ans;
+        
     }
 };
