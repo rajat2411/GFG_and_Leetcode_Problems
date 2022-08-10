b3f1d70e41fd7bf510fdc1af9eecb3f160c89cb6
RAJAT <65409221+rajat2411@users.noreply.github.com>
2022-08-10 23:56:44 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (32.60%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> generate(int n) {
-        vector<vector<int>> r(n);
+        vector<vector<int>>ans(n);
         for(int i=0;i<n;i++){
-
-        r[i].resize(i+1);// everytime row increase by 1
-            r[i][0]=r[i][i]=1;// first and last always 1
-            
+            ans[i].resize(i+1);
+            ans[i][0]=ans[i][i]=1;
             for(int j=1;j<i;j++){
-                r[i][j]=r[i-1][j-1]+r[i-1][j];
+                ans[i][j]=ans[i-1][j]+ans[i-1][j-1];
             }
         }
-        return r;
+        return ans;
     }
 };
