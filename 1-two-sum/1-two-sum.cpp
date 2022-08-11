97a136101062ddfe8803dc1d4908ce92878da1cf
RAJAT <65409221+rajat2411@users.noreply.github.com>
2022-08-12 00:23:56 +0530

    Time: 12 ms (88.86%), Space: 11 MB (24.23%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n=nums.size();
-        vector<int > ans;
-        // brute force approach 
-        // for(int i=0;i<n;i++){
-        //     for(int j=i+1;j<n;j++){
-        //         if(nums[i]==target-nums[j]){
-        //             ans.push_back(i);
-        //             ans.push_back(j);
-        //             break;
-        //         }
-        //     }
-        // }
-        // return ans;
-        
-        // use of hashind 
-        unordered_map<int,int>mpp;
-        for(int i=0;i<n;i++){
-            if(mpp.find(target-nums[i])!=mpp.end()){
+        vector<int> ans;
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            if(mp.find(target-nums[i])!=mp.end()){
                 ans.push_back(i);
-                ans.push_back(mpp[target-nums[i]]);
+                ans.push_back(mp[target-nums[i]]);
                 break;
+                
             }
-            mpp[nums[i]]=i;
+            mp[nums[i]]=i;
         }
         return ans;
-        
     }
 };
