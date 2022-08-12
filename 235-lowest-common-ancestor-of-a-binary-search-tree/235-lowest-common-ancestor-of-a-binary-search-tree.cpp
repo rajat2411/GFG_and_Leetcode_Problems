5eb551ea4820398684170b991bc62b604126746f
RAJAT <65409221+rajat2411@users.noreply.github.com>
2022-08-12 12:12:11 +0530

    Time: 73 ms (5.37%), Space: 23.1 MB (91.99%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -12,7 +12,7 @@ class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
          if(!root){
-            return NULL;
+            return root;
         }
         
         if(root->val < p->val && root->val< q->val){
@@ -22,7 +22,7 @@ public:
            return lowestCommonAncestor(root->left,p,q);
         }
         
-        
-            return root;
+        return root;
+            ;
     }
 };
