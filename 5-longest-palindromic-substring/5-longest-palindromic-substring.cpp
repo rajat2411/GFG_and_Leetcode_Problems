bdec72e06b5be1cb212896f24ddac3d0d94dc3b1
RAJAT <65409221+rajat2411@users.noreply.github.com>
2022-08-25 23:57:41 +0530

    Time: 1817 ms (5.01%), Space: 449.9 MB (6.16%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,22 +1,24 @@
-class Solution 
-{
+class Solution {
 public:
-    string longestPalindrome(string s1) 
-    {
-          string s2=s1;
+    string longestPalindrome(string s1) {
+        // i will use LCA technique 
+        string s2=s1;
         reverse(s2.begin(),s2.end());
         int n=s1.size();
         int m=s2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0)); 
-        int ans=0,index=0;
-        string result;
+        int ans=0;
+        string result="";
+        
+        
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-
                 if(s1[i-1]==s2[j-1]){
-                   dp[i][j]=1+dp[i-1][j-1]; 
+                    // if same 
+                    dp[i][j]=1+dp[i-1][j-1];
                     if(ans<dp[i][j]){
-                       string temp=s1.substr(i-dp[i][j],dp[i][j]);
+                        // dp ij tk ki string bna luga taaki palindrome check kr sku
+                        string temp=s1.substr(i-dp[i][j],dp[i][j]);
                         string temp2=temp;
                         reverse(temp2.begin(),temp2.end());
                         if(temp==temp2){
@@ -24,12 +26,15 @@ public:
                             result=temp;
                         }
                     }
+                   
+                    
                 }
-                else{
-                    dp[i][j]=0;
-                }
+                 else{
+                        dp[i][j]=0;
+                    }
             }
         }
-    return result;
+        return result;
+        
     }
 };
