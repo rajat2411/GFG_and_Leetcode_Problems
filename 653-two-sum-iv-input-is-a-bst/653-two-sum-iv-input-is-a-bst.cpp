710a4c0f0cf992a308454be33310661769f5894d
RAJAT <65409221+rajat2411@users.noreply.github.com>
2022-09-10 16:31:25 +0530

    Time: 73 ms (41.53%), Space: 36.9 MB (62.18%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -1,41 +1,53 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+class BSTiterator{
+    stack<TreeNode*>st;
+    bool reverse=true;
+    
+    public:
+        BSTiterator(TreeNode* root, bool isReverse){
+            
+            reverse=isReverse;
+            pushAll(root);
+        }
+    
+    int next(){
+        TreeNode* temp=st.top();
+        st.pop();
+        if(!reverse) pushAll(temp->right);
+        else pushAll(temp->left);
+        return temp->val;
+    }
+    bool hasNext(){
+        return !st.empty();
+    }
+    
+    private: 
+        void pushAll(TreeNode* node){
+            while(node!=NULL){
+                st.push(node);
+                if(reverse==true){
+                    node=node->right;
+                }
+                else{
+                    node=node->left;
+                }
+            }
+        }
+    
+};
 class Solution {
 public:
-    
-    void preorder(vector<int>&arr , TreeNode* root){
-        if(!root){
-            return ;
-        }
-        preorder(arr,root->left);
-        arr.push_back(root->val);
-        preorder(arr,root->right);
-        
-    }
     bool findTarget(TreeNode* root, int k) {
-        vector<int>arr;
-        preorder(arr,root);
-       int l=0,r=arr.size()-1;
-        while(l<r){
-            if(arr[l]+arr[r]==k){
-                return true;
-            }
-            else if(arr[l]+arr[r]>k){
-                r--;
-            }
-            else{
-                l++;
-            }
-            
+        if(!root) return false;
+        BSTiterator l(root,false);
+        BSTiterator r(root,true);
+        
+        int i=l.next();
+        int j=r.next();
+        
+        while(i<j){
+            if(i+j==k) return true;
+            else if(i+j<k) i=l.next();
+            else j=r.next();
         }
         return false;
     }
