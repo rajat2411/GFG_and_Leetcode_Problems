54241500a57faf421c3b32b6416d7b5a612ac304
RAJAT <65409221+rajat2411@users.noreply.github.com>
2022-09-11 17:19:30 +0530

    Time: 25 ms (7.89%), Space: 13.3 MB (25.59%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,46 +1,55 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        // using bfs 
-        if(grid.empty()) return 0;
+        // solving striver sde sheet
         int n=grid.size();
         int m=grid[0].size();
-        int tot=0,cnt=0,time=0;
-        queue<pair<int,int>>rotten;
+        // visited arry
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        queue<pair<pair<int,int>,int>> q;
+        
+        int cntFresh=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]!=0) tot++;
                 if(grid[i][j]==2){
-                    rotten.push({i,j});
+                    q.push({{i,j},0});
+                    vis[i][j]=2;
                 }
+                else if(grid[i][j]==1) 
+                    cntFresh++;
+                    vis[i][j]=1;
             }
         }
-        // till now i have rotten orange coordinates and total oranges 
-        int dx[4]={0,0,-1,1};
-        int dy[4]={1,-1,0,0};
         
-        while(!rotten.empty()){
-            int k=rotten.size();
-            cnt+=k;
-            while(k--){
-                pair<int,int>p=rotten.front();
-                rotten.pop();
-                int x=p.first;
-                int y=p.second;
-                for(int i=0;i<4;i++){
-                    int nx=x+dx[i];
-                    int ny=y+dy[i];
+        int time=0;
+        int drow[]={0,0,-1,+1};
+        int dcol[]={1,-1,0,0};
+        int cnt=0;
+        while(!q.empty()){
+            int row=q.front().first.first;
+            int col=q.front().first.second;
+            int t=q.front().second;
+            time=max(time,t);
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nrow=row+drow[i];
+                int ncol=col+dcol[i];
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && vis[nrow][ncol]==1 && grid[nrow][ncol]==1 ){
+                    vis[nrow][ncol]=2;
+                    q.push({{nrow,ncol},t+1});
+                    cnt++;
                     
-                    if(nx<0 || ny<0 || nx>=n || ny>=m || grid[nx][ny]!=1){
-                        continue;
-                    }
-                    grid[nx][ny]=2;
-                    rotten.push({nx,ny});
                 }
             }
-            if(!rotten.empty()) time++;
+            
             
         }
-        return (tot==cnt)?time:-1;
+        if(cnt==cntFresh){
+            return time;
+        }
+        else{
+            return -1;
+        }
+        
     }
 };
