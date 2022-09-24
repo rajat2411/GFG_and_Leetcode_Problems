35831ff5792c13c18bd16ea008de4775ebf9c047
RAJAT <65409221+rajat2411@users.noreply.github.com>
2022-09-25 00:18:00 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (78.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -16,7 +16,19 @@ public:
         return dp[index]=first+second;
     }
     int fib(int n) {
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
+        // vector<int>dp(n+1,-1);
+        // return solve(n,dp);
+        int dp[n+2];
+        dp[0]=0;
+        dp[1]=1;
+        
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+            
+        }
+        
+        return dp[n];
+        
+        
     }
 };
