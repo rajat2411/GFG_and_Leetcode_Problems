c29108edf02ee6ce05b0e920601f8f6d0b1d4f40
RAJAT <65409221+rajat2411@users.noreply.github.com>
2022-09-25 22:27:22 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (27.25%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,99 +1,16 @@
 class Solution {
 public:
-    
-    // memoization
-//     int uni(int m,int n,vector<vector<int>>&dp){
-//         if(m==0 && n==0){
-//             return 1;
-//         }
-        
-//         if(m<0 || n<0){
-//             return 0;
-//         }
-//         if(dp[m][n]!=-1){
-//             return dp[m][n];
-//         }
-        
-//         // accumulate
-//         int left=uni(m,n-1,dp);
-//         int up=uni(m-1,n,dp);
-//         return dp[m][n]=left+up;
-//     }
-    
-    // int uniquePaths(int m, int n) {
-    //     // declareing dp of m*n 
-    //     vector<vector<int>>dp(m,vector<int>(n,-1));
-    //     return uni(m-1,n-1,dp);
-    // }
-    
-    
-    
-//     =======>>>>>> tabulation<<<<<<<<============
-//      int uniquePaths(int m, int n) {
-//         // declareing dp of m*n 
-//         // vector<vector<int>>dp(m,vector<int>(n,-1));
-//          int dp[m][n];
-//         // return uni(m-1,n-1,dp);
-         
-//          for(int i=0;i<m;i++){
-//              for(int j=0;j<n;j++){
-//                  if(i==0 && j==0){
-//                      dp[i][j]=1;
-//                  }
-//                  else{
-//                      int up=0;
-//                      int left=0;
-//                      if(j>0){
-//                          left=dp[i][j-1];
-//                      }
-//                      if(i>0){
-//                          up=dp[i-1][j];
-//                      }
-//                      dp[i][j]=left+up;
-//                  }
-//              }
-//          }
-//          return dp[m-1][n-1];
-         
-//     }
-    
-    
-    
-    
-//     =================>>>>>space optimise<<<<<<<<<<<<<<==========
-    int uniquePaths(int m, int n) {
-        // declareing dp of m*n 
-        // vector<vector<int>>dp(m,vector<int>(n,-1));
-         // int dp[m][n];
-        // return uni(m-1,n-1,dp);
-        vector<int>prev(n,0);
-         
-         for(int i=0;i<m;i++){
-             vector<int>curr(n,0);
-             for(int j=0;j<n;j++){
-                 if(i==0 && j==0){
-                     curr[j]=1;
-                 }
-                 else{
-                     int up=0;
-                     int left=0;
-                     if(j>0){
-                         left=curr[j-1];
-                     }
-                     if(i>0){
-                         up=prev[j];
-                     }
-                     curr[j]=left+up;
-                 }
-             }
-             prev=curr;
-         }
-         return prev[n-1];
-         
+    int f(int i,int j,vector<vector<int>>&dp){
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int left=f(i-1,j,dp);
+        int up=f(i,j-1,dp);
+        return dp[i][j]=left+up;
     }
     
-    
-    
-    
-    
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return f(m-1,n-1,dp);
+    }
 };
