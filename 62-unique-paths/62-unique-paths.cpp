1bf6240593f4338df079ff422c2f988465998999
RAJAT <65409221+rajat2411@users.noreply.github.com>
2022-09-25 22:54:17 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (59.83%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -12,22 +12,26 @@ public:
     int uniquePaths(int m, int n) {
         // vector<vector<int>>dp(m,vector<int>(n,-1));
         // tabulation 
-        int dp[m][n];
-        dp[0][0]=1;
+        // int dp[m][n];
+        // dp[0][0]=1;
+        vector<int> prev(n,0);
+        
         for(int i=0;i<m;i++){
+            vector<int> curr(n,0);
             for(int j=0;j<n;j++){
-                if(i==0 && j==0) dp[0][0]=1;
+                if(i==0 && j==0) curr[j]=1;
               else  { 
                 int left=0;
-                  if(i>0)left=dp[i-1][j];
+                  if(i>0)left=prev[j];
                 int up=0;
-                  if(j>0) up=dp[i][j-1];
-                dp[i][j]=left+up;}
+                  if(j>0) up=curr[j-1];
+                curr[j]=left+up;}
             }
+            prev=curr;
         }
         
         
         // return f(m-1,n-1,dp);
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
